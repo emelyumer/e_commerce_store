de97890e9e378b54c4ee0a538fba1bdc6204a5e4
emelyumer <emi_92rs@abv.bg>
2024-04-14 20:37:45 +0300

    setup

--- account/views.py
+++ account/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from account.forms import CreateUserForm
 
 
 def register(request):
-    return HttpResponse('Account url set up')
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('')
+
+    contex = {'form': form}
+
+    return render(request, 'account/registration/register.html', context=contex)
--- e_commerce_store/settings.py
+++ e_commerce_store/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'cart',
     'account',
     'mathfilters',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
